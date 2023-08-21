3941b8672a4b677d3b487764b5a58a8b48bc7f18
Phan Đăng Lưu <51915789+DieepjAnhr@users.noreply.github.com>
2023-08-21 15:15:48 +0700

    update auth redis

--- api/configs/redis.config.js
+++ api/configs/redis.config.js
@@ -2,25 +2,41 @@ const redis = require('redis');
 let redisClient;
 
 (async () => {
-    redisClient = redis.createClient();
+    redisClient = redis.createClient({
+        host: '127.0.0.0',
+        port: 6379,
+        password: 'eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81'
+    });
     redisClient.on('error', (error) => console.error(`Error Redis: ${error}`));
     await redisClient.connect();
     console.log('redis connect success!');
 })();
 
-const getCache = async (key) => {
-    const cache = await redisClient.get(key);
-    return JSON.parse(cache);
-}
-
 const setCache = async (key, value, time) => {
-    await redisClient.set(key, JSON.stringify(value), { EX: time });
-    return true;
+    try {
+        await redisClient.set(key, JSON.stringify(value), { EX: time });
+        return true;
+    } catch (error) {
+        console.log(error);
+    }
 };
 
+const getCache = async (key) => {
+    try {
+        const cache = await redisClient.get(key);
+        return JSON.parse(cache);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 const clearCache = async (key) => {
-    await redisClient.del(key);
-    return true;
+    try {
+        await redisClient.del(key);
+        return true;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 module.exports = {
--- api/services/post.service.js
+++ api/services/post.service.js
@@ -1,9 +1,12 @@
 const { AppDataSource } = require("../configs/pg.config");
+const { setCache, getCache, clearCache } = require("../configs/redis.config");
 
 class PostService {
     constructor () {}
     async getAll(query) {
         try {
+            const cache = await getCache('post-' + JSON.stringify(query));
+            if (cache) return cache;
             const filter = {};
             if (query?.id) filter.id = Number(query.id);
             const result = await (await AppDataSource).getRepository('posts').find({
@@ -12,6 +15,7 @@ class PostService {
                     isDeleted: false 
                 }
             });
+            setCache('post-' + JSON.stringify(query), result, 5 * 60);
             return result;
         } catch (error) {
             throw error;
@@ -19,7 +23,10 @@ class PostService {
     }
     async getById(postId) {
         try {
+            const cache = await getCache('post-' + JSON.stringify(postId));
+            if (cache) return cache;
             const result = await (await AppDataSource).getRepository('posts').findOneBy({ id: Number(postId), isDeleted: false });
+            setCache('post-' + JSON.stringify(postId), result, 5 * 60);
             return result;
         } catch (error) {
             throw error;
@@ -41,6 +48,7 @@ class PostService {
             }
             const updateData = { ...oldData, ...newData };
             const result = await (await AppDataSource).getRepository('posts').save(updateData);
+            clearCache('post-*');
             return result;
         } catch (error) {
             throw error;
@@ -54,6 +62,7 @@ class PostService {
             }
             const updateData = { ...oldData, isDeleted: true };
             const result = await (await AppDataSource).getRepository('posts').save(updateData);
+            clearCache('post-*');
             return result;
         } catch (error) {
             throw error;
